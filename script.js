57c22b80bda02603f7c402052aab1484831d2e8a
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2021-12-06 19:15:08 +0100

    Added basic structures for testing.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log();
+/* Coins array */
+
+/* Tamagotchi svg */
+
+/* Buy button */
+
+/* Change tie button */
+
+/* Sell button */
+
+/* Crypto for your thoughts button */
+
+const cryptoButton = document.querySelector(".crypto-button");
+
+cryptoButton.addEventListener("click", () => {
+  window.alert(
+    "There are no thoughts or feelings in crypto boys. Continue mining."
+  );
+});
+
+/* Scroll event */
