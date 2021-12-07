c213c1636080b25d6e6ee760901cdf6d46bf219e
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2021-12-07 12:03:51 +0100

    Added js for coin stack.

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 /* Coins array */
 
+let coins = [];
+
+const coinsContainer = document.querySelector(".coins-container");
+const coinImage = "./images/coin.svg";
+
 /* Tamagotchi svg */
 
 /* Buy button */
 
+const buyButton = document.querySelector(".buy-button");
+
+buyButton.addEventListener("click", () => {
+  coins.push("Coin");
+  const newCoin = document.createElement("img");
+  newCoin.src = coinImage;
+  newCoin.classList.add("coin-styling");
+
+  coinsContainer.appendChild(newCoin);
+
+  // console.log(coins);
+  if (coins.length > 9) {
+    coins = [];
+    while (coinsContainer.firstChild) {
+      coinsContainer.removeChild(coinsContainer.firstChild);
+    }
+  }
+});
+
 /* Change tie button */
 
 /* Sell button */
+
+const sellButton = document.querySelector(".sell-button");
+
+// console.log(lastCoin);
+
+sellButton.addEventListener("click", () => {
+  coins.pop("Coin");
+  // console.log(coins);
+  const lastCoin = document.querySelector(".coin-styling:last-child");
+  // console.log(lastCoin);
+  if (coins.length > 0) {
+    coinsContainer.removeChild(lastCoin);
+  }
+});
 
 /* Crypto for your thoughts button */
 
